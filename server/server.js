69645707dc5d243e397cda11a96e99f5564be7ad
Muskan Kumar <muskankumar952593@gmail.com>
2025-11-17 04:09:53 +0530

    Ingest error fixed

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import 'dotenv/config';
 import connectDB from "./configs/db.js";
 import {inngest, functions} from './ingest/index.js'
+import {serve} from 'inngest/express'
 
 const app = express();
 
